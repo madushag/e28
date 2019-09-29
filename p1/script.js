0821d9f55cd4d6b320e33758a680c3b3aa139d43
Madusha Gamage <madusha.gamage@gmail.com>
2019-09-29 17:13:34 +0530

    Interim work on P1

--- p1/script.js
+++ p1/script.js
@@ -1,77 +1,83 @@
-var computerChoiceText;
+// The Vue instance
+let app = new Vue({
+  // Options
+  el: '#app',
+  data: {
+		isAdmin : true,
+        playerName: "",
+		guess: null,
+        'guesses': []
+    },
+	methods: {
+		playGame : function (event) {
+			var choices = ["Rock", "Paper", "Scissors"];
+			var userChoice = document.querySelector('input[name="choices"]:checked');
 
-function bodyLoaded(){
-    document.getElementById("btnPlay").addEventListener("click", playGame);
-}
+			if(userChoice == null){
+				alert("Please choose one option...");
+				return;
+			}
+			else{
+				var userChoiceText = userChoice.value;               
+				var computerChoice = Math.floor(Math.random() * 3);           
+				computerChoiceText = choices[computerChoice];
+				
+				var winner = determineWinner(userChoiceText, computerChoiceText);
 
-function playGame(){    
-    var choices = ["Rock", "Paper", "Scissors"];
-    var userChoice = document.querySelector('input[name="choices"]:checked');
+				document.getElementById("computerChoice").innerHTML = computerChoiceText;
+				
+				var resultText;
+				if(winner != "tie"){
+					resultText = "The winner is the " + winner + ".";
+				}
+				else{
+					resultText = "The game is a tie.";
+				}
+					
+				document.getElementById("result").innerHTML = resultText;
+				document.getElementById("divResult").className = "visible";
+			}    
+		},
+		determineWinner : function (userChoiceText, computerChoiceText) {
+			var winner = "";
 
-    if(userChoice == null){
-        alert("Please choose one option...");
-        return;
-    }
-    else{
-        var userChoiceText = userChoice.value;               
-        var computerChoice = Math.floor(Math.random() * 3);           
-        computerChoiceText = choices[computerChoice];
-        
-        var winner = determineWinner(userChoiceText, computerChoiceText);
+			if(userChoiceText == "Paper"){
+				if(computerChoiceText == "Rock"){
+					winner = "user";
+				}
+				else if(computerChoiceText == "Scissors") {
+					winner = "computer";
+				}
+				else {
+					winner = "tie";
+				}
+			}
 
-        document.getElementById("computerChoice").innerHTML = computerChoiceText;
-        
-        var resultText;
-        if(winner != "tie"){
-            resultText = "The winner is the " + winner + ".";
-        }
-        else{
-            resultText = "The game is a tie.";
-        }
-            
-        document.getElementById("result").innerHTML = resultText;
-        document.getElementById("divResult").className = "visible";
-    }    
-}
+			if(userChoiceText == "Rock"){
+				if(computerChoiceText == "Scissors"){
+					winner = "user";
+				}
+				else if(computerChoiceText == "Paper") {
+					winner = "computer";
+				}
+				else {
+					winner = "tie";
+				}
+			}
+			
+			if(userChoiceText == "Scissors"){
+				if(computerChoiceText == "Paper"){
+					winner = "user";
+				}
+				else if(computerChoiceText == "Rock") {
+					winner = "computer";
+				}
+				else {
+					winner = "tie";
+				}
+			}
 
-function determineWinner(userChoiceText, computerChoiceText){
-    var winner = "";
-
-    if(userChoiceText == "Paper"){
-        if(computerChoiceText == "Rock"){
-            winner = "user";
-        }
-        else if(computerChoiceText == "Scissors") {
-            winner = "computer";
-        }
-        else {
-            winner = "tie";
-        }
-    }
-
-    if(userChoiceText == "Rock"){
-        if(computerChoiceText == "Scissors"){
-            winner = "user";
-        }
-        else if(computerChoiceText == "Paper") {
-            winner = "computer";
-        }
-        else {
-            winner = "tie";
-        }
-    }
-    
-    if(userChoiceText == "Scissors"){
-        if(computerChoiceText == "Paper"){
-            winner = "user";
-        }
-        else if(computerChoiceText == "Rock") {
-            winner = "computer";
-        }
-        else {
-            winner = "tie";
-        }
-    }
-
-    return winner;
-}
+			return winner;
+		}
+	}
+})
