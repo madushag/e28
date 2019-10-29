e85a8c480a09766ebf5582b89aa40cd611ef59c8
Madusha Gamage <madusha.gamage@gmail.com>
2019-10-28 22:00:15 -0400

    ready to push to prod

---
+++ week8/src/components/RoundDetail.vue
@@ -0,0 +1,39 @@
+<template>
+    <div v-bind:class="{ winner : winner == 'Player'}">
+        <ul>
+            <li>Round number : {{ number }}</li>
+            <li>Winner: {{ winner }}</li>
+            <li>
+                <slot name="choice"></slot>
+            </li>
+            <li>
+                <slot name="coin"></slot>
+            </li>
+        </ul>
+        <button @click="$emit('delete-round', number)">Delete round</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {};
+    },
+    props: {
+        number: {
+            type: Number,
+            default: 0
+        },
+        winner: {
+            type: String,
+            default: ""
+        }
+    }
+};
+</script>
+
+<style scoped>
+.winner {
+    background-color: green;
+}
+</style>
