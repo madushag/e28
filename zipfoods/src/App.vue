f0b3cce1e13d32e41d4bbeeea96f918ef57a8fe7
Madusha Gamage <madusha.gamage@gmail.com>
2019-11-04 20:48:58 -0500

    updated router to use names

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -5,7 +5,7 @@
         <nav>
             <ul>
                 <li v-for='link in links' :key='link'>
-                    <router-link exact :to='paths[link]'>{{ link }}</router-link>
+                    <router-link exact :to="{ name: link }">{{ link }}</router-link>
                 </li>
             </ul>
         </nav>
@@ -24,12 +24,7 @@ export default {
         return {
             products: products,
             // src/App.vue data properties
-            links: ['home', 'products', 'categories'],
-            paths: {
-                home: '/',
-                products: '/products',
-                categories: '/categories'
-            }
+            links: ['home', 'products', 'categories']            
         };
     }
 };
